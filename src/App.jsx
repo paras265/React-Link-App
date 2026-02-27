1e5bc17526281a9229092b9c77882249bc22894f
paras vaghela <parasvaghela000@gmail.com>
2026-02-27 13:43:37 +0530

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Profile from "./componente/Profile";
 
-
-function Home() {
-  return <h1>HOME PAGE</h1>;
-}
-
 function App() {
-  
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/user-info" element={<Profile />} />
-    </Routes>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Profile />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
